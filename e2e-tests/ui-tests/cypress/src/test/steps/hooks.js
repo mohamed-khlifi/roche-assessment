82fb9a6d179f4f9005209aa867bdeadc764f09bc
Mohamed Khlifi <mohamed.khlifi@bexapp.de>
2024-11-03 13:54:48 +0100

    removing unused hooks

--- e2e-tests/ui-tests/cypress/src/test/steps/hooks.js
+++ e2e-tests/ui-tests/cypress/src/test/steps/hooks.js
@@ -2,16 +2,9 @@ import { Before, After } from 'cypress-cucumber-preprocessor/steps';
 import {LoginPage} from "../../pages/LoginPage";
 
 let loginPage;
-Before(function () {
-
-});
 
 Before({ tags: "not @login" }, () => {
     loginPage = new LoginPage();
     loginPage.goToLoginPage();
     loginPage.loginWithValidCredentials();
-});
-
-After(() => {
-    cy.log('Running teardown after each scenario');
 });
